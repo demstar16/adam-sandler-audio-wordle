e912bf982271e8fc288ada3222fd944d006738d5
demstar16 <44187160+demstar16@users.noreply.github.com>
2023-10-10 13:12:21 +0800

    edit database.py to work for this proj

--- app/database.py
+++ app/database.py
@@ -10,57 +10,56 @@ def create_table():
     cursor = connection.cursor()    
 
     sql_command = (
-        """CREATE TABLE IF NOT EXISTS games_played (
+        """CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY,
             user_id TEXT NOT NULL,
             date DATE NOT NULL,
             score INTEGER NOT NULL
         )"""
     )
-
-    print(sql_command)
     cursor.execute(sql_command)
     connection.commit()
 
 
-def insert_record(user_id, date, score):
-    """Attempts to store data on a played game.  Will only allow 1 insert per 
-    user_id per day to avoid manipualting user stats.
+def update_record(user_id, date, score):
+    """Attempts to update an existing record for a user's game.
 
     Parameters:
     user_id (str): UUID for each user
     date (datetime): day the game was played
-    win (int): if player won the  game win == 1, otherwise win==0
-    attempts (int): number of attempts made in the game
+    score (int): new score to update
 
     Returns:
-    int: last row id
+    bool: True if the record was updated, False otherwise
     """
 
-    
     connection = sqlite3.connect(
         "database.db",
         detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES
     )
     cursor = connection.cursor()
 
-    cursor.execute("SELECT * FROM games_played WHERE user_id==? AND date==?", (user_id, date))
+    cursor.execute("SELECT * FROM users WHERE user_id=? AND date=?", (user_id, date))
 
     # Only allow 1 submission per day
     todays_submissions = cursor.fetchall()
     if len(todays_submissions) < 1:
+        # A record already exists for this user and date, so update it
         sql_command = (
-            f"""INSERT INTO games_played(user_id, date, score) VALUES (
-                ?,
-                ?,
-                ?,
-                ?
-            )
-            """
+            """UPDATE users SET score=? WHERE user_id=? AND date=?"""
+        )
+        cursor.execute(sql_command, (score, user_id, date))
+        connection.commit()
+        return True
+    else:
+        # No existing record found, so insert a new one
+        sql_command = (
+            """INSERT INTO users(user_id, date, score) VALUES (?, ?, ?)"""
         )
         cursor.execute(sql_command, (user_id, date, score))
         connection.commit()
-    return cursor.lastrowid
+        return False
+
 
 def get_records(user_id):
     """Retrieves records for particular user_id
@@ -80,10 +79,12 @@ def get_records(user_id):
     cursor = connection.cursor()
 
     # gets list of top 10 users' scores, in desc order
-    cursor.execute(f"SELECT user_id, score FROM games_played ORDER BY score DESC LIMIT 10;") 
+    cursor.execute(f"SELECT user_id, score FROM users ORDER BY score DESC LIMIT 10;") 
     top10 = cursor.fetchall()
-    # get number of games played by this user      
-    cursor.execute(f"SELECT score FROM games_played WHERE user_id==?", (user_id,))
-    user_score = cursor.fetchall()
+    
+    # get user score    
+    cursor.execute(f"SELECT score FROM users WHERE user_id==?", (user_id,))
+    user_score_raw = cursor.fetchall()
+    user_score = user_score_raw[0][0]
 
     return top10, user_score
