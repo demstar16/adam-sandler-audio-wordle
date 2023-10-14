f0f559a86d00404a2201bc8ec95bc46914ac3f20
demstar16 <44187160+demstar16@users.noreply.github.com>
2023-10-14 15:24:54 +0800

    Fix to display more info

--- app/static/js/leaderboard.js
+++ app/static/js/leaderboard.js
@@ -1,10 +1,31 @@
-    let user_score_div = document.getElementById('user_score');
+    // -----------User Stats ---------------------
+    let user_stats_div = document.getElementById('user_stats');
+    let dummy1 = document.getElementById('data1');
+    let userStatsInvalidString = dummy1.textContent;
+    let userStatsValidString = userStatsInvalidString.replace(/'/g, '"');
+    userStatsValidString = userStatsValidString.replace('(', '[');
+    userStatsValidString = userStatsValidString.replace(')', ']');
+    userStatsValidString = userStatsValidString.replace(/None/g, 'null');
+    console.log(userStatsInvalidString);
+    console.log(userStatsValidString);
+    let user_stats = JSON.parse(userStatsValidString);
+    let username;
+    if (user_stats[0][1] != null) {
+        username = user_stats[0][1];
+    } else {
+        username = user_stats[0][0];
+    }
+
+    let score = user_stats[0][2];
+    let text = document.createElement('p');
+    text.innerHTML = "<b>Your Score</b> (" + username + "): <b>" + score + "</b>";
+
+    user_stats_div.appendChild(text);
+
+    // ------------ TOP 10 --------------------
     // my not so efficient way of getting the data
     let dummy = document.getElementById('data');
     let top10_div = document.getElementById('top10');
-
-    let user_score = document.createElement('p');
-    user_score.innerText = user_score_div.getAttribute('userScore');
 
     let top10InvalidString = dummy.textContent;
     console.log(top10InvalidString);
@@ -12,27 +33,33 @@
     top10ValidString = top10ValidString.replace(/None/g, 'null');
     let top10 = JSON.parse(top10ValidString);
 
-    let text = document.createElement('p');
-    text.innerText = "Your Score: ";
-
-    let score_div = document.createElement('div');
-    score_div.className = 'yourScore'
-
-    score_div.appendChild(text);
-    score_div.appendChild(user_score);
-    user_score_div.appendChild(score_div);
-
+    let rank = 1
     top10.forEach(function(item) {
-        let username = document.createElement('p');
-        username.innerText = item[0];
+        let rankString = rank + ".) ";
 
+        let nameElement = document.createElement('p');
+        let name;
+        if (item[1] != null) 
+        {
+            name = item[1];
+        }
+        else 
+        {
+            name = item[0];
+        }
+        if (name == username){
+            nameElement.innerHTML = "<b>" + rankString + "</b><b>" + name + "</b>";
+        } else {
+            nameElement.innerHTML = rankString + name;
+        }
         let score = document.createElement('p');
-        score.innerText = item[1];
+        score.innerText = item[2];
 
         let score_row = document.createElement('div')
         score_row.className = 'score_row'
 
-        score_row.appendChild(username);
+        score_row.appendChild(nameElement);
         score_row.appendChild(score);
-        top10_div.appendChild(score_row)
+        top10_div.appendChild(score_row);
+        rank = rank + 1;
     });
