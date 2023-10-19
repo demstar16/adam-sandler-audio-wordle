0803b0ef94949589adfa70c6f1aa30e76f11e4c3
demstar16 <44187160+demstar16@users.noreply.github.com>
2023-10-19 10:12:24 +0800

    add clips

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -13,6 +13,26 @@ audioData = [
     ['https://www.youtube.com/embed/GHZSYBkKec4?enablejsapi=1', 'Grown Ups'],
     ['https://www.youtube.com/embed/MV4zHRnIUpU?enablejsapi=1', 'Anger Management'],
     ['https://www.youtube.com/embed/LqXS0XYHB-Y?enablejsapi=1', 'The Wedding Singer'],
+    ['https://www.youtube.com/embed/7I2-14y6-jMenablejsapi=1', 'The Waterboy'],
+    ['https://www.youtube.com/embed/JjJSk9tmduAenablejsapi=1', 'The Waterboy'],
+    ['https://www.youtube.com/embed/0J0F3hqZZyEenablejsapi=1', 'The Longest Yard'],
+    ['https://www.youtube.com/embed/yRp_Qx3CjR0enablejsapi=1', 'The Longest Yard'],
+    ['https://www.youtube.com/embed/pc_TH7-HfYIenablejsapi=1', 'The Longest Yard'],
+    ['https://www.youtube.com/embed/qmzxR0zfIN0enablejsapi=1', 'I Now Pronounce You Chuck & Larry'],
+    ['https://www.youtube.com/embed/pIgTnfJEfr0enablejsapi=1', 'I Now Pronounce You Chuck & Larry'],
+    ['https://www.youtube.com/embed/_Y8lpIzstdUenablejsapi=1', 'I Now Pronounce You Chuck & Larry'],
+    ['https://www.youtube.com/embed/RX09g003p5cenablejsapi=1', 'Little Nicky'],
+    ['https://www.youtube.com/embed/Bq-Bas9zeNMenablejsapi=1', 'Jack and Jill'],
+    ['https://www.youtube.com/embed/fcH0Uxyu1KMenablejsapi=1', 'Jack and Jill'],
+    ['https://www.youtube.com/embed/zbXT6NPKWgwenablejsapi=1', 'Bedtime Stories'],
+    ['https://www.youtube.com/embed/c6xQI6sddX0enablejsapi=1', 'That`s My Boy'],
+    ['https://www.youtube.com/embed/6t-ljfykXzMenablejsapi=1', 'That`s My Boy'],
+    ['https://www.youtube.com/embed/_-ea8diZ5M8enablejsapi=1', 'That`s My Boy'],
+    ['https://www.youtube.com/embed/ykBG9mW1yC4enablejsapi=1', 'That`s My Boy'],
+    ['https://www.youtube.com/embed/xrT1QYVVD8kenablejsapi=1', 'Blended'],
+    ['https://www.youtube.com/embed/6OutSzM_eC8enablejsapi=1', 'Blended'],
+    ['https://www.youtube.com/embed/S__SZwFn0Rwenablejsapi=1', '50 First Dates'],
+    
 ];
 
 movies = [
@@ -25,14 +45,22 @@ movies = [
     `Grown Ups 2`,
     `Click`,
     `Happy Gilmore`,
-    `Hubie Halloween`
+    `Hubie Halloween`,
+    'The Waterboy',
+    'The Longest Yard',
+    'I Now Pronounce You Chuck & Larry',
+    'Little Nicky',
+    'Jack and Jill',
+    'Pixels',
+    'Bedtime Stories',
+    'That`s My Boy',
+    'Blended'
 ];
 
 let indexDiv = document.getElementById('index');
 let index = indexDiv.getAttribute("index");
 console.log(index)
 let guesses = 0;
-let gameover = false;
 let points = 0;
 // global variable for the player
 var player;
@@ -88,68 +116,66 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 function submit() 
 {
+    let date = new Date(new Date().getFullYear(),new Date().getMonth() , new Date().getDate())
     guesses += 1;
-    checkSubmissions().then((gameover) => 
-    // This code will execute after the response is received.
-    {
-        // audio for button click
-        let sound = document.getElementById('myAudio');
+    // audio for button click
+    let sound = document.getElementById('myAudio');
 
-        // get the index of the selected option
-        let selectedIndex = dropdown.selectedIndex;
-        // get a selected option and text value using the text property
-        let selectedValue = dropdown.options[selectedIndex].text;
-        
-        let result = document.getElementById('result');
-        result.innerHTML = null;
-        let p = document.createElement('p');
-        let text;
-        
-        if (gameover) 
+    // get the index of the selected option
+    let selectedIndex = dropdown.selectedIndex;
+    // get a selected option and text value using the text property
+    let selectedValue = dropdown.options[selectedIndex].text;
+    
+    let result = document.getElementById('result');
+    result.innerHTML = null;
+    let p = document.createElement('p');
+    let text;
+
+    console.log(played_today)
+    if (played_today) 
+    {
+        sound.src = "static/audio/done.mp3";
+        text = "You're done for the day... come back tomorrow"
+    }
+    else if (selectedValue == audioData[index][1]) 
+    {
+        if (guesses == 1)
         {
-            sound.src = "static/audio/done.mp3";
-            text = "You're done for the day... come back tomorrow"
+            points = 100;
         }
-        else if (selectedValue == audioData[index][1]) 
+        else if ( guesses == 2)
         {
-            if (guesses == 1)
-            {
-                points = 100;
-            }
-            else if ( guesses == 2)
-            {
-                points = 50;
-            }
-            else if (guesses == 3)
-            {
-                points = 25;
-            }
-            sound.src = "static/audio/win.mp3";
-            text = '<p>You`re RIGHT! Gotta respect that!<br>You earned <b>' + points + '</b> points.</p>';
+            points = 50;
+        }
+        else if (guesses == 3)
+        {
+            points = 25;
+        }
+        sound.src = "static/audio/win.mp3";
+        text = '<p>You`re RIGHT! Gotta respect that!<br>You earned <b>' + points + '</b> points.</p>';
+        localStorage.setItem("last_game", date);
+        sendStats(points);
+    }
+    else
+    {
+        if (guesses == 3) 
+        {
+            sound.src = "static/audio/loss.mp3";
+            text = '<p>That`s GAMEOVER for today</p>';
+            localStorage.setItem("last_game", date);
             sendStats(points);
         }
-        else
+        else 
         {
-            if (guesses == 3) 
-            {
-                sound.src = "static/audio/loss.mp3";
-                text = '<p>That`s GAMEOVER for today</p>';
-                sendStats(points);
-            }
-            else 
-            {
-                sound.src = "static/audio/try-again.mp3";
-                text = '<p>Try Again Pal</p>';
-            }
+            sound.src = "static/audio/try-again.mp3";
+            text = '<p>Try Again Pal</p>';
         }
-        p.innerHTML = text;
-        result.appendChild(p);
-        player.pauseVideo();
-        sound.load();
-        sound.play();
-    }).catch((error) => {
-        console.error(error); // Handle errors
-    });
+    }
+    p.innerHTML = text;
+    result.appendChild(p);
+    player.pauseVideo();
+    sound.load();
+    sound.play();
 }
 
 function sendStats(points) {
